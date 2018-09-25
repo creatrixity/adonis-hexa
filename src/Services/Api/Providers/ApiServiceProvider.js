e2af735e47f142288b240ab0481294873b0b03fe
creatrixity <creatrixity@gmail.com>
2018-09-25 13:55:33 +0100

    add ApiServiceProvider class

---
+++ src/Services/Api/Providers/ApiServiceProvider.js
@@ -0,0 +1,75 @@
+/*
+ * adonis-hexa
+ *
+ * Contributor: Caleb Mathew <creatrixity@gmail.com>
+ *
+*/
+"use strict";
+
+const FoundationServiceProvider = use(
+  "Src/Foundation/FoundationServiceProvider"
+);
+const Routes = use("Src/Services/Api/Routes");
+
+/**
+ * Registers and sets upthe internals of this service.
+ *
+ * **Namespace**: `Src/Services/Api/Providers/ApiServiceProvider` <br />
+ * **Singleton**: No <br />
+ * **Alias**: None
+ *
+ * @class ApiServiceProvider
+ */
+class ApiServiceProvider extends FoundationServiceProvider {
+  /**
+   * Registers bindings for this service. It is invoked at startup.
+   *
+   * @method register
+   *
+   * @return {Void}
+   */
+  register() {
+    const self = this;
+
+    Routes.map(route => {
+      let { controller } = route;
+
+      self.registerControllerBinding(controller);
+    });
+  }
+
+  /**
+   * Registers route controllers for this service. Sets up routes when our app boots.
+   *
+   * @method boot
+   *
+   * @return {Void}
+   */
+  boot() {
+    this.app
+      .use("Route")
+      .group(() => {
+        const self = this;
+
+        Routes.map(routeItem => {
+          let { route, controller, handler, method } = routeItem;
+
+          let isProtected =
+            typeof routeItem.protected === "undefined"
+              ? false
+              : routeItem.protected;
+
+          self.registerRouteController(
+            route,
+            controller,
+            handler,
+            method,
+            isProtected
+          );
+        });
+      })
+      .prefix("/api/v1/");
+  }
+}
+
+module.exports = ApiServiceProvider;
