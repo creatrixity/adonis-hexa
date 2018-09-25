b3c4c8290cad229f412a58afcdbaf9d4036d8afb
creatrixity <creatrixity@gmail.com>
2018-09-25 13:46:24 +0100

    add FetchUserJob class

---
+++ src/Domains/User/FetchUserJob.js
@@ -0,0 +1,48 @@
+/*
+ * adonis-hexa
+ *
+ * Contributor: Caleb Mathew <creatrixity@gmail.com>
+ *
+*/
+"use strict";
+
+const BaseJob = use("Src/Foundation/BaseJob");
+const UserRepository = use("Src/Data/Repositories/UserRepository");
+const User = use("App/Models/User");
+
+/**
+ * Fetches a user.
+ *
+ * **Namespace**: `Src/Domains/User/FetchUserJob` <br />
+ * **Singleton**: No <br />
+ * **Alias**: None
+ *
+ * @class FetchUserJob
+ * @constructor
+ */
+class FetchUserJob extends BaseJob {
+  constructor(params) {
+    super(params);
+  }
+
+  /**
+   * Contains code that will be ran when this job is invoked.
+   *
+   * @method handle
+   *
+   * @return {Object} Lucid/ORM
+   */
+  async handle() {
+    const userRepo = new UserRepository();
+
+    const user = await User.query()
+      .where({ username: this.params.uid })
+      .orWhere({
+        email: this.params.uid
+      });
+
+    return user;
+  }
+}
+
+module.exports = FetchUserJob;
