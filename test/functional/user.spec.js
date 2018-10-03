cc724e420764b8f7c87b2ea37267fcce60423ab8
creatrixity <creatrixity@gmail.com>
2018-10-03 12:39:54 +0100

    add functional test to user test suite

--- test/functional/user.spec.js
+++ test/functional/user.spec.js
@@ -34,3 +34,36 @@ test("Should register a user through the HTTP client", async ({
     }
   });
 }).timeout(0);
+
+test("Should return a list of users through the HTTP client", async ({
+  client,
+  assert
+}) => {
+  const response = await client.get(`/api/v1/users`).end();
+
+  response.assertStatus(200);
+
+  assert.isAtLeast(response.body.length, 1);
+}).timeout(0);
+
+test("Should return details about user #1 through the HTTP client", async ({
+  client,
+  assert
+}) => {
+  let data = {
+    username: TEST_USERNAME,
+    email: TEST_EMAIL,
+    password: TEST_PASSWORD
+  };
+
+  let { username, email } = data;
+
+  const response = await client.get(`/api/v1/users/1`).end();
+
+  response.assertStatus(200);
+
+  response.assertJSONSubset({
+    username,
+    email
+  });
+}).timeout(0);
